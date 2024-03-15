92c1dda45e233fe65ea12c9321df8a7a9a612ddb
Mathilde RESSIER <mathilde@unicorp.dev>
2024-03-15 11:25:46 +0100

    Init repository

---
+++ Tests/Unicorp.APIBuilderTests/Unicorp_APIBuilderTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Unicorp_APIBuilder
+
+final class Unicorp_APIBuilderTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
